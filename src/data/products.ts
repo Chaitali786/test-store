b8cad6551e7a0d63e9dd0cc017e6ba587edc8ff7
Chaitali Deore <chaitalideore@gmail.com>
2026-08-29 08:31:47 +0200

    basic vite project setup is done

---
+++ src/data/products.ts
@@ -0,0 +1,26 @@
+import { Product } from "../../types/products";
+
+export const products: Product[] = [
+  {
+    id: "1",
+    name: "Smart Watch",
+    price: "$100",
+    image:
+      "https://images.unsplash.com/photo-1590658268037-6bf12165a8df?w=300&auto=format&fit=crop",
+  },
+  {
+    id: "2",
+    name: "Wireless Headphones",
+    price: "$75",
+    image:
+      "https://images.unsplash.com/photo-1620916566398-39f1143ab7be?w=300&auto=format&fit=crop",
+  },
+  {
+    id: "3",
+    name: "Running Shoes",
+    price: "$50",
+    image:
+      "https://images.unsplash.com/photo-1576995853123-5a10305d93c0?w=300&auto=format&fit=crop",
+  },
+ 
+];
